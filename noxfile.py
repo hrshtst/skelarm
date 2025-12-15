c0c668c0e257485a0d530d9513d812de6c2d6d68
Hiroshi Atsuta <atsuta@ieee.org>
2025-12-15 22:20:26 +0900

    feat: enable python 3.13 and 3.14 support in nox tests

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.12", "3.13", "3.14"])
 def tests(session: nox.Session) -> None:
     """Run the test suite."""
     # Install dev dependencies for testing
